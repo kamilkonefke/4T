30fac3a8131ba038679ef7a6ec9c0ca5ecb127d1
Kamil Konefke <kamilkonefke@proton.me>
2025-05-19 13:25:10 +0200

    TABLICEEEE

--- Programowanie/Tablice/main.js
+++ Programowanie/Tablice/main.js
@@ -34,38 +34,91 @@ var liczby = [3, 6, 17, 7, 2, 9, 3, 4, 3, 4];
     // let czy_istnieje = liczby.includes(user) ? "tak" : "nie";
     // console.log(czy_istnieje);
 
-const logi = "admin/40/10-12-2020#tomasz/16/11-12-2020#janusz99/5/11-12-2020#urszula/18/12-12-2020#admin/23/15-12-2020";
+// const logi = "admin/40/10-12-2020#tomasz/16/11-12-2020#janusz99/5/11-12-2020#urszula/18/12-12-2020#admin/23/15-12-2020";
 
-const logs_raw = logi.split("#");
-const logs = [];
+// const logs_raw = logi.split("#");
+// const logs = [];
 
-for (let i = 0; i < logs_raw.length; i++) {
-    logs.push(logs_raw[i].split("/"));
-}
+// for (let i = 0; i < logs_raw.length; i++) {
+//     logs.push(logs_raw[i].split("/"));
+// }
 
-// -------------------------
-let suma_minut = 0;
-let max_min = 0;
-let max_min_index = 0;
-for (let i = 0; i < logs.length; i++) {
-    suma_minut += parseFloat(logs[i][1]);
+// console.log(logs);
 
-    if (max_min < parseFloat(logs[i][1])) {
-        max_min = parseFloat(logs[i][1]);
-        max_min_index = i;
-    }
-}
+// // -------------------------
+// let suma_minut = 0;
+// let max_min = 0;
+// let max_min_index = 0;
+// for (let i = 0; i < logs.length; i++) {
+//     suma_minut += parseFloat(logs[i][1]);
 
-console.log(`Najdluzej zalogowany byl: ${logs[max_min_index][0]} \nZalogowany byl przez: ${logs[max_min_index][1]} min`);
-console.log(`Wszystkie godziny łacznie: ${(suma_minut/60).toFixed(1)}`)
+//     if (max_min < parseFloat(logs[i][1])) {
+//         max_min = parseFloat(logs[i][1]);
+//         max_min_index = i;
+//     }
+// }
 
-let user = prompt("Kogo godziny chcesz sprawdzić?");
+// console.log(`Najdluzej zalogowany byl: ${logs[max_min_index][0]} \nZalogowany byl przez: ${logs[max_min_index][1]} min`);
+// console.log(`Wszystkie godziny łacznie: ${(suma_minut/60).toFixed(1)}`)
 
-let user_time_sum = 0;
-for (let i = 0; i < logs.length; i++) {
-    if (logs[i][0] == user) {
-        user_time_sum += parseFloat(logs[i][1]);
-    }
-}
+// let user = prompt("Kogo godziny chcesz sprawdzić?");
 
-console.log(`${user} był łącznie zalogwany pzez ${user_time_sum} min`);
+// let user_time_sum = 0;
+// for (let i = 0; i < logs.length; i++) {
+//     if (logs[i][0] == user) {
+//         user_time_sum += parseFloat(logs[i][1]);
+//     }
+// }
+
+// console.log(`${user} był łącznie zalogwany pzez ${user_time_sum} min`);
+
+
+// function ciag() {
+//     var ciag_liczb = [1, 1, 0, 0, 1, 1, 0, 0];
+//     let x1 = ciag_liczb.slice(0, ciag_liczb.length / 2.0);
+//     let x2 = ciag_liczb.slice(ciag_liczb.length / 2.0, ciag_liczb.length);
+
+//     if (ciag_liczb.length % 2 != 0) {
+//         console.log("Ciag nie jest parzysty")
+//         return;
+//     }
+
+//     for (let i = 0; i < ciag_liczb.length / 2.0; i++) {
+//         if (x1[i] != x2[i]) {
+//             console.log("Nie jest ciagiem");
+//             return;
+//         }
+//     }
+
+//     console.log("Pojedynczy ciag ma")
+//     console.log(ciag_liczb.length / 2.0)
+// }
+
+// function zakupy() {
+//     const cennik = ["ogorki:4", "jablka:3", "gruszki:7.5"];
+
+//     const przedmioty = [];
+//     const ilosc = [];
+//     var suma = 0;
+
+//     var is_input = true;
+//     while(is_input) {
+//         const user = prompt("Produkt=ilość");
+//         if (user.toString() == 'x') {
+//             is_input = false;
+//         }
+
+//         var a = user.split("=");
+//         przedmioty.push(a[0])
+//         ilosc.push(a[1])
+//     }
+
+//     for (let i = 0; i < przedmioty.length - 1.0; i++) {
+//         var ae = cennik[i].split(':');
+
+//         console.log(`${i+1}. ${przedmioty[i]} x${ilosc[i]} | ${ae[1]*ilosc[i]} zł`);
+//         suma += parseFloat(ae[1]*ilosc[i]);
+//     }
+
+//     console.log(`SUMA: ${suma} zł`);
+// }
